2cbaec408c2664f45f013ebabb0d8b201cd8de4f
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-02-24 22:11:00 -0600

    Update ej11.c

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,16 @@
+&a[0] = 1000
+&a[1] = 1008
+&a[2] = 1016
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 11
-    return 0;
-}
+/*
+1. Tamaño del tipo:
+El incremento es de 8 bytes entre cada elemento,
+por lo tanto sizeof(tipo) = 8 bytes.
+
+2. Tipo posible:
+Un tipo de dato posible en C que ocupa 8 bytes es double
+(también podría ser long long en muchos compiladores).
+
+3. Fórmula usada:
+&a[i] = &a[0] + i * sizeof(tipo)
+*/
