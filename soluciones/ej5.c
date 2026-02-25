ca2f524690883a7d7f2a031dcd295c2ae653bf94
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-02-24 22:45:11 -0600

    Update ej5.c

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -1,6 +1,28 @@
+/*
+Ejercicio 5
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 5
-    return 0;
-}
+Un estudiante afirma:
+Si a[0] = 1000 entonces a[10] = 1010.
+
+Formula general de direccionamiento:
+Dir(a[i]) = Dir(a[0]) + i * sizeof(tipo)
+
+Sustituyendo i = 10:
+
+Dir(a[10]) = 1000 + 10 * sizeof(tipo)
+
+Caso 1: si sizeof(tipo) = 1 (por ejemplo char)
+Dir(a[10]) = 1000 + 10 * 1
+           = 1010
+En este caso la afirmacion seria correcta.
+
+Caso 2: si sizeof(tipo) = 4 (por ejemplo int)
+Dir(a[10]) = 1000 + 10 * 4
+           = 1000 + 40
+           = 1040
+En este caso la afirmacion es incorrecta.
+
+Conclusion:
+La afirmacion no es correcta en general.
+Solo seria valida si el tamaño del tipo fuera 1 byte.
+*/
