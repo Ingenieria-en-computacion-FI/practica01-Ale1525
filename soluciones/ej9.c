99fb2f976b2c249ed6e83d297b1434048b29e47a
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-02-24 22:46:34 -0600

    Update ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,10 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 9
+
+int main() {
+    int a[5];
+
+    printf("&a[-1] = %p\n", &a[-1]);
+    printf("&a[5]  = %p\n", &a[5]);
+
     return 0;
 }
